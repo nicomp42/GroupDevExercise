30bfccb939195dd2799a3342900b4eb3062c7c68
Brad <bruceba@mail.uc.edu>
2018-01-31 18:42:50 -0500

    Updated SumOfDigitsIsPrime.cs. Authors: Will Watson and Brad Bruce

--- SumOfDigitsIsPrimeClass.cs
+++ SumOfDigitsIsPrimeClass.cs
@@ -18,7 +18,28 @@ namespace GroupDevExercise
         /// <returns>true if all tests passed, false otherwise.</returns>
         override public Boolean Test(Boolean verbose)
         {
-            return false;
+            if (!SumOfDigitsIsPrime(25)) return false;//return 7
+
+            if (!SumOfDigitsIsPrime(30)) return false;//return 3
+
+            if (!SumOfDigitsIsPrime(65)) return false;//return 11
+
+            if (!SumOfDigitsIsPrime(2999)) return false;//return 29
+
+            if (!SumOfDigitsIsPrime(887)) return false;//return 25
+
+            if (!SumOfDigitsIsPrime(999999999994)) return false;//return 103
+
+            if (SumOfDigitsIsPrime(44)) return false;//return 8
+
+            if (SumOfDigitsIsPrime(99)) return false;//return 18
+
+            if (SumOfDigitsIsPrime(12829)) return false;//return
+
+            if (SumOfDigitsIsPrime(1289)) return false;//return
+
+
+            return true;
         }
         /// <summary>
         /// Sum the digits in an integer and check that number for primeness
@@ -27,7 +48,17 @@ namespace GroupDevExercise
         /// <returns>true if the sum of digits is prime, false otherwise</returns>
         public static bool SumOfDigitsIsPrime(long num)
         {
-            return false;
+            string StringNum = num.ToString();
+            long Sum = 0;
+            foreach (char c in StringNum){
+           
+                Sum += (long)char.GetNumericValue(c);
+
+            }//end foreach
+
+            return IsPrimeClass.IsPrime(Sum);
+    
         }
     }
 }
+
