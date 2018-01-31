6f0eb6ad27f89540b4a8361dc93cddf2c4f696f5
Bill Nicholson <nicholdw@ucmail.uc.edu>
2018-01-31 14:57:42 -0500

    Deleted some logic that no one should see

--- IsPrimeClass.cs
+++ IsPrimeClass.cs
@@ -19,29 +19,7 @@ namespace GroupDevExercise {
         /// <param name="verbose">True if the method should print test results as they are processed</param>
         /// <returns>true if all tests passed, false otherwise.</returns>
         public static Boolean Test(Boolean verbose) {
-            long[] testCasesNotPrime = new long[] {  4, 6, 100, 999, 1000 };
-            long[] testCasesPrime = new long[] { 2, 3, 5, 101 };
-            int testsFailed = 0, testsPassed = 0; 
-            // Run the tests of non-prime numbers
-            for (int i = 0; i < testCasesNotPrime.Length; i++) {
-                if (IsPrime(testCasesNotPrime[i])) {
-                    if (verbose) { Console.WriteLine(testCasesNotPrime[i] + " is not prime but was reported as prime."); }
-                    testsFailed++;
-                } else { testsPassed++; }
-            }
-            // Run the tests of prime numbers
-            for (int i = 0; i < testCasesPrime.Length; i++) {
-                if (!IsPrime(testCasesPrime[i])) {
-                    if (verbose) { Console.WriteLine(testCasesPrime[i] + " is prime but was reported as not prime."); }
-                    testsFailed++;
-                }
-                else { testsPassed++; }
-            }
-            if (verbose) {
-                Console.WriteLine(testsFailed + " tests failed.");
-                Console.WriteLine(testsPassed + " tests passed.");
-            }
-            return ((testsFailed > 0) ? false : true);
+            return false;
         }
 
         /// <summary>
